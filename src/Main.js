bee53122fc2a45d0388ce90b09469bdb8e6de5ae
Dylan Taft <dylbronjames@gmail.com>
2019-08-20 22:59:01 -0600

    changes

--- src/Main.js
+++ src/Main.js
@@ -1,13 +1,13 @@
 import React, { Fragment } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import App from './App';
-import EditForm from './components/employee/EditForm';
+import EmployeeForm from './components/employee/EmployeeForm';
 
 const Main = () => (
   <Fragment>
     <Switch>
       <Route exact path="/" component={App} />
-      <Route exact path="/edit" component={EditForm} />
+      <Route exact path="/edit" component={EmployeeForm} />
     </Switch>
   </Fragment>
 )
